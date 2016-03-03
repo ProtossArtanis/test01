391050e9c27878aed7548b133a5618dbbed589c5
ProtossArtanis <steinsgate@mac24.local>
2016-03-03 11:14:42 +0800

    Initial Commit

---
+++ test01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test01
+//
+//  Created by SteinsGate on 16/3/3.
+//  Copyright © 2016年 SteinsGate. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
